c1a118efb3478f84ffb923a51132c420def2fb03
stephipie <stephi.pietschmann@gmail.com>
2024-10-01 13:47:58 +0200

    add first task from homework python basics, setting variables

---
+++ python-basics.py
@@ -0,0 +1,5 @@
+# Setting variables / Ducktyping
+fist_name = "Annika"        #string
+last_name = "Mustermann"    #string
+birth_year = 1995           #integer
+has_license = True          #boolean (True/False)
