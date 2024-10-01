310cfd106406bfc51618bad85d64c2280e3424e2
stephipie <stephi.pietschmann@gmail.com>
2024-10-01 14:41:42 +0200

    add task two of homework

---
+++ maths-operations.py
@@ -0,0 +1,12 @@
+# Define two variables 
+a = 10
+b = 3
+
+# Calculation and output
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Integer quotient:", a // b)
+print("Modulo:", a % b)
+print("a high b:", a **b)
+
